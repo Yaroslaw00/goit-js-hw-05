1fed91f8ae0c99bf77e95f1c241cb6731ce03e51
Yaroslaw00 <yarikost901@gmail.com>
2026-04-18 22:10:46 +0200

    ок

--- index.js
+++ index.js
@@ -19,6 +19,8 @@
 // console.log(Account.prototype.getInfo);
 // mango.getInfo();
 // poly.getInfo();
+
+
 // // 2
 // class User {
 //   constructor({ name, age, followers }) {
@@ -46,115 +48,160 @@
 // poly2.getInfo();
 
 // 3
-class Car {
-  static getSpecs(car) {
-    console.log(
-      `${car.maxSpeed},${car.speed}, ${car.isOn}, ${car.distance} и ${car.price}`,
-    );
+class Storage {
+  // Конструктор приймає початковий масив товарів
+  constructor(items) {
+    this.items = items;
   }
 
-  constructor({ maxSpeed, price }) {
-    this.maxSpeed = maxSpeed;
-    this.price = price;
-    this.speed = 0;
-    this.isOn = false;
-    this.distance = 0;
-  }
-  /*
-   * Додай статичний метод `getSpecs(car)`,
-   * який приймає об'єкт-машину як параметр і виводить
-   * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
-   */
-
-  /*
-   * Конструктор отримує об'єкт налаштувань.
-   *
-   * Додай властивості майбутнього екземпляра класу:
-   * speed - поточна швидкість, початкова 0
-   * price - ціна автомобіля
-   * maxSpeed - максимальна швидкість
-   * isOn - заведений автомобіль, значення true або false. Спочатку false
-   * distance - загальний кілометраж, спочатку 0
-   */
-
-  /*
-   * Додай геттер і сеттер для властивості price,
-   * який буде працювати з властивістю ціни автомобіля.
-   */
-
-  /*
-   * Додай код для того, щоб завести автомобіль
-   * Записує у властивість isOn значення true
-   */
-  turnOn() {
-    this.isOn = true;
+  // Повертає масив поточних товарів
+  getItems() {
+    return this.items;
   }
 
-  /*
-   * Додай код для того, щоб заглушити автомобіль
-   * Записує у властивість isOn значення false,
-   * і скидає поточну швидкість в 0
-   */
-  turnOff() {
-    this.isOn = false;
-    this.speed = 0;
+  // Додає новий товар до масиву
+  addItem(item) {
+    this.items.push(item);
   }
 
-  /*
-   * Додає до властивості speed отримане значення,
-   * за умови, що результуюча швидкість
-   * не більше, ніж значення властивості maxSpeed
-   */
-  accelerate(value) {
-    const newSpeed = this.speed + value;
-    if (this.newSpeed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    } else {
-      this.speed = newSpeed;
-    }
-  }
-
-  /*
-   * Забирає від властивості speed отримане значення,
-   * за умови, що результуюча швидкість не менше нуля
-   */
-  decelerate(value) {
-    const lowSpeed = this.speed - value;
-    if (lowSpeed >= 0) {
-      this.speed = lowSpeed;
-    } else {
-      // Якщо ж після віднімання виходить "мінус",
-      // зазвичай швидкість просто скидають до нуля
-      this.speed = 0;
-    }
-  }
-  /*
-   * Додає в поле distance кілометраж (hours * speed),
-   * але тільки в тому випадку, якщо машина заведена!
-   */
-  drive(hours) {
-    if (this.isOn === true) {
-      this.distance += hours * this.speed;
+  // Видаляє товар, якщо він є у списку
+  removeItem(item) {
+    const itemIndex = this.items.indexOf(item);
+    
+    // Перевіряємо, чи товар існує в масиві (index не дорівнює -1)
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
     }
   }
 }
 
-const mustang = new Car({ maxSpeed: 200, price: 2000 });
+// --- Перевірка роботи коду ---
 
-mustang.turnOn();
-mustang.accelerate(50);
-mustang.drive(2);
+const storage = new Storage([
+  'Нанітоїди',
+  'Пролонгер',
+  'Залізні жупи',
+  'Антигравітатор',
+]);
 
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+const items = storage.getItems();
+console.table(items); // Початковий список
 
-mustang.decelerate(20);
-mustang.drive(1);
-mustang.turnOff();
+storage.addItem('Дроїд');
+console.table(storage.items); // Список після додавання 'Дроїд'
 
-Car.getSpecs(mustang);
-// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+storage.removeItem('Пролонгер');
+console.table(storage.items); // Список після видалення 'Пролонгер'
+// 5
+// class Car {
+//   static getSpecs(car) {
+//     console.log(
+//       `${car.maxSpeed},${car.speed}, ${car.isOn}, ${car.distance} и ${car.price}`,
+//     );
+//   }
 
-console.log(mustang.price); // 2000
-mustang.price = 4000;
-console.log(mustang.price); // 4000
+//   constructor({ maxSpeed, price }) {
+//     this.maxSpeed = maxSpeed;
+//     this.price = price;
+//     this.speed = 0;
+//     this.isOn = false;
+//     this.distance = 0;
+//   }
+//   /*
+//    * Додай статичний метод `getSpecs(car)`,
+//    * який приймає об'єкт-машину як параметр і виводить
+//    * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
+//    */
+
+//   /*
+//    * Конструктор отримує об'єкт налаштувань.
+//    *
+//    * Додай властивості майбутнього екземпляра класу:
+//    * speed - поточна швидкість, початкова 0
+//    * price - ціна автомобіля
+//    * maxSpeed - максимальна швидкість
+//    * isOn - заведений автомобіль, значення true або false. Спочатку false
+//    * distance - загальний кілометраж, спочатку 0
+//    */
+
+//   /*
+//    * Додай геттер і сеттер для властивості price,
+//    * який буде працювати з властивістю ціни автомобіля.
+//    */
+
+//   /*
+//    * Додай код для того, щоб завести автомобіль
+//    * Записує у властивість isOn значення true
+//    */
+//   turnOn() {
+//     this.isOn = true;
+//   }
+
+//   /*
+//    * Додай код для того, щоб заглушити автомобіль
+//    * Записує у властивість isOn значення false,
+//    * і скидає поточну швидкість в 0
+//    */
+//   turnOff() {
+//     this.isOn = false;
+//     this.speed = 0;
+//   }
+
+//   /*
+//    * Додає до властивості speed отримане значення,
+//    * за умови, що результуюча швидкість
+//    * не більше, ніж значення властивості maxSpeed
+//    */
+//   accelerate(value) {
+//     const newSpeed = this.speed + value;
+//     if (this.newSpeed > this.maxSpeed) {
+//       this.speed = this.maxSpeed;
+//     } else {
+//       this.speed = newSpeed;
+//     }
+//   }
+
+//   /*
+//    * Забирає від властивості speed отримане значення,
+//    * за умови, що результуюча швидкість не менше нуля
+//    */
+//   decelerate(value) {
+//     const lowSpeed = this.speed - value;
+//     if (lowSpeed >= 0) {
+//       this.speed = lowSpeed;
+//     } else {
+//       // Якщо ж після віднімання виходить "мінус",
+//       // зазвичай швидкість просто скидають до нуля
+//       this.speed = 0;
+//     }
+//   }
+//   /*
+//    * Додає в поле distance кілометраж (hours * speed),
+//    * але тільки в тому випадку, якщо машина заведена!
+//    */
+//   drive(hours) {
+//     if (this.isOn === true) {
+//       this.distance += hours * this.speed;
+//     }
+//   }
+// }
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
